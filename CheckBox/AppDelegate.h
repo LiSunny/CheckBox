d0e21266d630139fc1d06f6a5488e0ee37f93d1a
Ccli <ccli321@163.com>
2016-08-03 11:12:19 +0800

    Initial Commit

---
+++ CheckBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CheckBox
+//
+//  Created by HeavenLi on 16/8/3.
+//  Copyright © 2016年 HeavenLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
